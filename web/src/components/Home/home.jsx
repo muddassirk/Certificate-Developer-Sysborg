853d25daf13e3211f58616e0a5fbc0f3e07bdfe6
Muhammad Muddassir Khan <mkkhanu72@gmail.com>
2021-03-10 14:55:55 +0500

    filtered

--- web/src/components/Home/home.jsx
+++ web/src/components/Home/home.jsx
@@ -20,14 +20,21 @@ import './css/style.css'
 
 
 
+let data = [];
 
 
 function Home() {
 
 
-    const [newposts, setNewPosts] = React.useState([]);
-    const [searchEmail, setsearchEmail] = React.useState('');
-    console.log(searchEmail);
+
+    // const [allEmails, setAllEmails] = React.useState([]);
+    const [filteredUser, setFilteredUser] = React.useState([]);
+    // const [result, setResult] = React.useState(true);
+    
+    const searchText = React.useRef(null);
+    
+    // console.log(searchEmail);
+    // console.log(filteredUser);
 
 
     React.useEffect(() => {
@@ -38,37 +45,45 @@ function Home() {
                 // console.log("response", res);
                 // console.log("response", res.data.Details);
                 // console.log("response", res.data.Details[0].studentName);
-                setNewPosts(res.data.Details)
-                console.log(newposts);
+                data = res.data.Details;
+                setFilteredUser (data)
                 // console.log(newposts[0].studentEmail);
-            
-
+                // console.log(newposts);
             });
 
             
-
-
-
     }, []);
+    
+    function handleSearchBtn() {
+        // filterPosts.map((e, i) => {
+        //     console.log(e.studentName);
+        //     setFilteredUser(e)
 
-    newposts.map((value,index)=>{
-        
-            console.log(value.studentEmail);
-        
-    })
+        // })
 
-    function seachBar(e){
+        if(!searchText.current.value) return;
+
+
+        const filterPosts = data.filter((post) => {
+            // return post.studentEmail === searchEmail
+          return post.studentEmail.toLowerCase().includes(searchText.current.value)
+        }
+        
+        )
+        setFilteredUser(filterPosts)
+        if(!searchText.current.value) return;
+
+
+
 
     }
 
 
-
+console.log('filter posts',filteredUser);
 
 
     return (
         <div>
-
-
 
             {/* Bottom Bar Start  */}
             <div className="bottom-bar">
@@ -82,10 +97,10 @@ function Home() {
                             </div>
                         </div>
                         <div className="col-md-6">
-                            <div className="search">
+                            <div className="search"> 
                                 {/* <input type="text" placeholder="Search" onChange={(e)=>console.log(e.target.value)}/> */}
-                                <input type="text" placeholder="Search" onChange={(e)=>setsearchEmail(e.target.value)}/>
-                                <button><i className="fa fa-search"></i></button>
+                                <input type="text" onChange={handleSearchBtn} placeholder="Search" ref={searchText} />
+                                {/* <button onClick={handleSearchBtn}><i className="fa fa-search"></i></button> */}
                             </div>
                         </div>
                         <div className="col-md-3">
@@ -98,18 +113,96 @@ function Home() {
 
             <div className="container">
 
-
-            {
-
-            }
+                {filteredUser.map((value, index) => {
 
 
+                  return (
+                      <div key={index} className='w-75 mx-auto'>
+                          <div className="post-bar">
+                              <div className="post_topbar">
+                                  <div
+                                      style={{
+                                          display: "flex",
+                                          justifyContent: "space-between"
+                                      }}>
+                                      <div>
+                                          <img style={{ width: "125px", borderRadius: "50%" }}
+                                              src={value.studentDp} alt="" />
+                                          <div className="usy-name">
+                                              <h3>{value.studentName}</h3>
+                                              <h3>{value.studentEmail}</h3>
+                                          </div>
+                                      </div>
+                                      <div >
+                                          <QRCode
+                                              value={"https://facebook.com/muddassir.perfect"}
+                                              size={128}
+                                              bgColor={"#ffffff"}
+                                              fgColor={"#000000"}
+                                          />
+                                      </div>
+                                  </div>
+
+                              </div>
+                              <div className="epi-sec">
+                                  <ul className="descp">
+                                      <li>Pakistan</li>
+                                  </ul>
+                              </div>
+                              <div className="job_descp">
+                                  <div style={{ lineHeight: "1.4em" }}>
+                                      <h6>Course Title: </h6>
+                                      <h6>Issue Date: {value.studentGrandDate} </h6>
+                                      <h6>Expiry Data: {value.studentExpireDate}</h6>
+                                  </div>
+                                  <ul className="job-dt">
+                                      <li><a >Experience</a></li>
+                                      <li><span>{value.studentExperience}</span></li>
+                                  </ul>
+                                  <p>
+                                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                                      luctus hendrerit metus, ut ullamcorper quam finibus at. Etiam id magna sit amet...
+                                      
+                                  </p>
+                                  <ul className="skill-tags">
+                                      <li><a>HTML</a></li>
+                                      <li><a>CSS</a></li>
+                                      <li><a>Javascript</a></li>
+                                      <li><a>Node.js</a></li>
+                                      <li><a>React</a></li>
+                                      <li><a>React Native</a></li>
+                                  </ul>
+                              </div>
+
+                          </div>
+
+
+                      </div>)
+
+                  })}
+                              
+                
 
 
 
 
-                {
-                    newposts.map((value, index) => {
+
+
+
+
+
+
+
+
+
+
+
+
+                {/* {
+                    filteredUser === {} ? 
+
+                    (newposts.map((value, index) => {
+
 
                         return (
 
@@ -117,17 +210,17 @@ function Home() {
                                 <div className="post-bar">
                                     <div className="post_topbar">
                                         <div
-                                        style={{
-                                            display:"flex",
-                                            justifyContent:"space-between"                                            
-                                        }}>
+                                            style={{
+                                                display: "flex",
+                                                justifyContent: "space-between"
+                                            }}>
                                             <div>
-                                            <img style={{ width: "125px", borderRadius:"50%" }}
-                                                src={value.studentDp} alt="" />
-                                            <div className="usy-name">
-                                                <h3>{value.studentName}</h3>
-                                                <h3>{value.studentEmail}</h3>
-                                            </div>
+                                                <img style={{ width: "125px", borderRadius: "50%" }}
+                                                    src={value.studentDp} alt="" />
+                                                <div className="usy-name">
+                                                    <h3>{value.studentName}</h3>
+                                                    <h3>{value.studentEmail}</h3>
+                                                </div>
                                             </div>
                                             <div >
                                                 <QRCode
@@ -144,40 +237,29 @@ function Home() {
                                         <ul className="descp">
                                             <li>Pakistan</li>
                                         </ul>
-                                        {/* <ul className="bk-links mb-4">
-                                            <li>Space for QR Code</li>
-                                            <li>
-                                                <QRCode
-                                                    value={"http://picturesofpeoplescanningqrcodes.tumblr.com/"}
-                                                    size={128}
-                                                    bgColor={"#ffffff"}
-                                                    fgColor={"#000000"}
-                                                />
-                                            </li>
-                                        </ul> */}
                                     </div>
                                     <div className="job_descp">
-                                        <div style={{lineHeight:"1.4em"}}>
-                                        <h6>Course Title: </h6>
-                                        <h6>Issue Date: {value.studentGrandDate} </h6>
-                                        <h6>Expiry Data: {value.studentExpireDate}</h6>
+                                        <div style={{ lineHeight: "1.4em" }}>
+                                            <h6>Course Title: </h6>
+                                            <h6>Issue Date: {value.studentGrandDate} </h6>
+                                            <h6>Expiry Data: {value.studentExpireDate}</h6>
                                         </div>
                                         <ul className="job-dt">
                                             <li><a >Experience</a></li>
                                             <li><span>{value.studentExperience}</span></li>
                                         </ul>
-                                        <p>     
+                                        <p>
                                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                                            luctus hendrerit metus, ut ullamcorper quam finibus at. Etiam id magna sit amet... 
-                                            {/* <a href="#" title>view more</a> */}
-                                            </p>
+                                            luctus hendrerit metus, ut ullamcorper quam finibus at. Etiam id magna sit amet...
+                                            
+                                        </p>
                                         <ul className="skill-tags">
-                                                    <li><a>HTML</a></li>
-                                                    <li><a>CSS</a></li>
-                                                    <li><a>Javascript</a></li>
-                                                    <li><a>Node.js</a></li>
-                                                    <li><a>React</a></li>
-                                                    <li><a>React Native</a></li>
+                                            <li><a>HTML</a></li>
+                                            <li><a>CSS</a></li>
+                                            <li><a>Javascript</a></li>
+                                            <li><a>Node.js</a></li>
+                                            <li><a>React</a></li>
+                                            <li><a>React Native</a></li>
                                         </ul>
                                     </div>
 
@@ -193,10 +275,74 @@ function Home() {
 
                         )
 
-                    })
+                    }))
+                    :
+                    (
+                        <div className='w-75 mx-auto'>
+                        <div className="post-bar">
+                            <div className="post_topbar">
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "space-between"
+                                    }}>
+                                    <div>
+                                        <img style={{ width: "125px", borderRadius: "50%" }}
+                                            src={filteredUser.studentDp} alt="" />
+                                        <div className="usy-name">
+                                            <h3>{filteredUser.studentName}</h3>
+                                            <h3>{filteredUser.studentEmail}</h3>
+                                        </div>
+                                    </div>
+                                    <div >
+                                        <QRCode
+                                            value={"https://facebook.com/muddassir.perfect"}
+                                            size={128}
+                                            bgColor={"#ffffff"}
+                                            fgColor={"#000000"}
+                                        />
+                                    </div>
+                                </div>
+    
+                            </div>
+                            <div className="epi-sec">
+                                <ul className="descp">
+                                    <li>Pakistan</li>
+                                </ul>
+                            </div>
+                            <div className="job_descp">
+                                <div style={{ lineHeight: "1.4em" }}>
+                                    <h6>Course Title: </h6>
+                                    <h6>Issue Date: {filteredUser.studentGrandDate} </h6>
+                                    <h6>Expiry Data: {filteredUser.studentExpireDate}</h6>
+                                </div>
+                                <ul className="job-dt">
+                                    <li><a >Experience</a></li>
+                                    <li><span>{filteredUser.studentExperience}</span></li>
+                                </ul>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                                    luctus hendrerit metus, ut ullamcorper quam finibus at. Etiam id magna sit amet...
 
-
-                }
+                                </p>
+                                <ul className="skill-tags">
+                                    <li><a>HTML</a></li>
+                                    <li><a>CSS</a></li>
+                                    <li><a>Javascript</a></li>
+                                    <li><a>Node.js</a></li>
+                                    <li><a>React</a></li>
+                                    <li><a>React Native</a></li>
+                                </ul>
+                            </div>
+    
+                        </div>
+    
+    
+                    </div>
+    
+                    )
+                
+                } */}
 
             </div>
 
